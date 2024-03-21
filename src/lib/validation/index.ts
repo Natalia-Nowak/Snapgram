6afc1b83eef7abc2f5fcf4d2c5951424997d6478
Natalia-Nowak <nowak.natalia.080@gmail.com>
2024-03-21 15:59:27 +0100

    improved signupform

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -3,4 +3,8 @@ import { z } from "zod";
 export const SignupValidationSchema = z.object({
   name: z.string().min(2, { message: "Too short" }),
   username: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
